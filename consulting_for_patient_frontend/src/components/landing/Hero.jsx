be1bb13b6571ba3b59270ca2528c396550e35d9e
Homoye_ghost <k.moh9315@gmail.com>
2025-11-14 23:27:10 +0000

    configuration des pages assigner 3

--- consulting_for_patient_frontend/src/components/landing/Hero.jsx
+++ consulting_for_patient_frontend/src/components/landing/Hero.jsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom'
 import { Button } from '../ui/button'
-
+import hospitalLogo from '../../assets/hopital.jpg'
 export const Hero = ({ title, description, buttonPrimary, buttonSecondary }) => {
   return (
-    <section className="relative py-20 md:py-32 lg:py-40 overflow-hidden">
-      <div className="absolute inset-0 bg-gradient-to-br from-primary/10 via-background to-primary/5"></div>
+    <section className="relative py-20 md:py-32 lg:py-40 overflow-hidden" style={{ backgroundImage: `url(${hospitalLogo})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <div className="absolute inset-0 bg-black/85 from-primary/10 via-background to-primary/5"></div>
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
         <div className="max-w-4xl mx-auto text-center">
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6 bg-gradient-to-r from-primary to-primary/70 bg-clip-text text-transparent">
